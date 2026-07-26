a60449f7eeca01937f58fe817467cf61ac6c82d8
Your Name <akusilda@proton.me>
2026-07-26 21:38:18 +0300

    Задание по шаблонизации выполнены

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from catalog import views
 from catalog.apps import CatalogConfig
 from catalog.views import home,contacts
 
@@ -7,4 +8,5 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path('',home, name='home'),
     path('contacts/',contacts, name='contacts'),
+    path('product/<int:pk>/', views.product_detail, name='product_detail'),
 ]
