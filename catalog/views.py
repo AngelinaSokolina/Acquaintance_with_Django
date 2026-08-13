e512c72c06a9589fb8b2b7a19e4e5b617f56c146
Your Name <akusilda@proton.me>
2026-08-13 15:23:05 +0300

    Готово: CRUD продуктов, валидация, стилизация формы

--- catalog/views.py
+++ catalog/views.py
@@ -1,11 +1,14 @@
-from django.views.generic import ListView, DetailView, TemplateView
+from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView
+from django.urls import reverse_lazy            #функция, которая превращает имя маршрута (например, catalog:home) в полный URL-адрес (например, /)
 from .models import Product
+from .forms import ProductForm
 
 
 class HomeListView(ListView):
     model = Product
     template_name = 'home.html'
     context_object_name = 'products'
+    ordering = ['name']
 
 
 class ProductDetailView(DetailView):
@@ -16,3 +19,20 @@ class ProductDetailView(DetailView):
 
 class ContactsView(TemplateView):
     template_name = 'contacts.html'
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/product_form.html'
+    success_url = reverse_lazy('catalog:home')      #«После успешного создания или редактирования товара перенаправь пользователя на главную страницу»
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/product_form.html'
+
+    #вернуться на страницу товара после редактирования
+    def get_success_url(self):
+        return reverse_lazy('catalog:product_detail', kwargs={'pk': self.object.pk})
